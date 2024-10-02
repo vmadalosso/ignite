86a2cce19d91ef55511f67cec9feb218cac1e75a
Vitor Madalosso <vitorconnected@gmail.com>
2024-10-02 02:09:17 -0400

    api rest nodejs com fastify

---
+++ nodejs/aulas/02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,26 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transacao de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
