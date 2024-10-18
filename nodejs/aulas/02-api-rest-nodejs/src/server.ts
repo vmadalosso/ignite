cd822de2b2beafdf9a6183098d337e6cf783e57f
Vitor Madalosso <vitorconnected@gmail.com>
2024-10-18 01:16:01 -0400

    fix: Error Deploy port

--- nodejs/aulas/02-api-rest-nodejs/src/server.ts
+++ nodejs/aulas/02-api-rest-nodejs/src/server.ts
@@ -1,10 +1,15 @@
 import { env } from './env'
 import { app } from './app'
 
+const PORT = env.PORT
+
 app
   .listen({
-    port: env.PORT,
+    port: PORT,
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    console.log(`HTTP Server Running on port ${PORT}!`)
+  })
+  .catch((err) => {
+    console.error('Error starting the server:', err)
   })
