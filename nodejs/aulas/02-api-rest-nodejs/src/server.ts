74a12b07929e44760e0ab84eb4dfc9450415cff1
Vitor Madalosso <vitorconnected@gmail.com>
2024-10-17 23:53:51 -0400

    test(e2e): add E2E tests for transactions listing

--- nodejs/aulas/02-api-rest-nodejs/src/server.ts
+++ nodejs/aulas/02-api-rest-nodejs/src/server.ts
@@ -1,21 +1,5 @@
-import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
-
-const app = fastify()
-
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transacao de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
-})
+import { app } from './app'
 
 app
   .listen({
