1177a2ded7a11f7dcb351899fcd8b2375d633cfd
Vitor Madalosso <vitorconnected@gmail.com>
2024-10-18 01:55:54 -0400

    fix: erro port render

--- nodejs/aulas/02-api-rest-nodejs/src/server.ts
+++ nodejs/aulas/02-api-rest-nodejs/src/server.ts
@@ -1,10 +1,16 @@
-import { env } from './env'
 import { app } from './app'
+
+// Converte o valor de PORT para número, se não estiver definido, usa 4000 como padrão
+const PORT = Number(process.env.PORT) || 4000
 
 app
   .listen({
-    port: env.PORT,
+    port: PORT,
   })
   .then(() => {
-    console.log(`HTTP Server Running on port ${env.PORT}!`)
+    console.log(`HTTP Server Running on port ${PORT}!`)
+  })
+  .catch((err) => {
+    console.error('Error starting the server:', err)
+    process.exit(1)
   })
